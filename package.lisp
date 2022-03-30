e694886196a91a9872f52b7410867ca62a6e2c30
Edward Langley <el-github@elangley.org>
2022-03-29 21:26:26 -0700

    chore(package): reformat

--- package.lisp
+++ package.lisp
@@ -6,20 +6,21 @@
 (defpackage :data-lens.lenses
   (:shadow :set)
   (:use :cl)
-  (:export :over :set :view :make-alist-lens :make-plist-lens :make-hash-table-lens
-           :make-list-lens))
+  (:export #:over #:set #:view #:make-alist-lens #:make-plist-lens
+           #:make-hash-table-lens #:make-list-lens))
 
 (defpackage :data-lens
   (:use :cl)
   (:import-from #:serapeum #:op #:defalias)
   (:export #:regex-match #:include #:exclude #:pick #:key-transform
-           #:combine #:derive #:cumsum #:over #:on #:shortcut #:defun-ct #:key
-           #:extract-key #:element #:let-fn #:juxt #:transform-tail #:slice
-           #:compress-runs #:combine-matching-lists #:sorted #:applicable-when
+           #:combine #:derive #:cumsum #:over #:on #:shortcut
+           #:defun-ct #:key #:extract-key #:element #:let-fn #:juxt
+           #:transform-tail #:slice #:compress-runs
+           #:combine-matching-lists #:sorted #:applicable-when
            #:of-length #:of-min-length #:of-max-length #:transform-head
-           #:maximizing #:zipping #:applying #:splice-elt #:transform-elt #:denest
-           #:op #:defalias #:<> #:<>1 #:== #:• #:suffixp #:functionalize #:inc
-           #:group-by #:keys))
+           #:maximizing #:zipping #:applying #:splice-elt
+           #:transform-elt #:denest #:op #:defalias #:<> #:<>1 #:== #:•
+           #:suffixp #:functionalize #:inc #:group-by #:keys))
 
 (defpackage :data-lens.transducers.internals
   (:use :cl)
@@ -29,13 +30,12 @@
 
 (defpackage :data-lens.transducers
   (:use :cl)
-  (:import-from :data-lens.transducers.internals
+  (:import-from #:data-lens.transducers.internals
                 #:unwrap #:init #:reduce-generic #:stepper #:transduce
                 #:exit-early #:into)
-  (:export #:mapping :filtering :deduping :catting :splitting
-           #:exit-early :taking :dropping :transduce
-           #:hash-table-builder :vector-builder :list-builder
-           #:collecting #:mv-mapping #:mv-selecting
-           #:hash-table-select #:mv-filtering #:mapcatting
-           #:lazy-sequence #:compressing-runs #:iota
-           #:repeating #:repeating* #:into))
+  (:export #:mapping #:filtering #:deduping #:catting #:splitting
+           #:exit-early #:taking #:dropping #:transduce
+           #:hash-table-builder #:vector-builder #:list-builder
+           #:collecting #:mv-mapping #:mv-selecting :hash-table-select
+           #:mv-filtering #:mapcatting :lazy-sequence
+           #:compressing-runs #:iota :repeating #:repeating* #:into))
